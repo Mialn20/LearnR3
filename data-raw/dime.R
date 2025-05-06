6462dea6c039903b6e879322364f4158cf7a3d59
Mikkel Alnor <Mikkelalnor@gmail.com>
2025-05-06 10:58:57 +0200

    Inital commit

---
+++ data-raw/dime.R
@@ -0,0 +1,17 @@
+library(here)
+library(fs)
+
+dime_link <- "https://github.com/rostools/r-cubed-intermediate/raw/refs/heads/main/data/dime.zip"
+
+download.file(dime_link, destfile = here("data-raw/dime.zip"))
+
+
+
+unzip(
+  here("data-raw/dime.zip"),
+  exdir = here("data-raw/dime/")
+)
+# Remove leftover folder so unzipping is always clean
+dir_delete(here("data-raw/dime"))
+
+
